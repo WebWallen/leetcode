fbe0fec77437910cfa19958cb130aa6925765a40
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-05-09 23:18:02 -0400

    Solved Remove Element

---
+++ arrays/remove-element.js
@@ -0,0 +1,9 @@
+var removeElement = function(nums, val) {
+    for(var i = nums.length - 1; i >= 0; i--){
+        if(nums[i] === val){
+            nums.splice(i, 1);
+        }
+    }
+};
+
+// It would not accept with a for loop starting at index 0 and I can't comprehend why
