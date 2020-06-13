0739df15566af2e8c32124af49ab9ea4bf33c40e
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-06-13 18:48:55 -0400

    Reverse linked list code

---
+++ linked_lists/reverse-linked-list.py
@@ -0,0 +1,10 @@
+def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+
+        while head:
+            cur = head
+            head = head.next
+            cur.next = prev
+            prev = cur
+
+        return prev
