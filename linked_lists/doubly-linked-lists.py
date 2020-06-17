3e72be00dbbf1c844c9c40bb639278bbd2314f0d
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-06-17 19:41:20 -0400

    Beginnings of doubly linked list exercises.

---
+++ linked_lists/doubly-linked-lists.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
