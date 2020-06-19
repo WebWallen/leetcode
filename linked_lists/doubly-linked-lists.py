f2d787a5f5e3fdd41c9ad4fcbcde4a1b6c100f5a
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-06-18 20:08:04 -0400

    Get node method added to DLL class.

--- linked_lists/doubly-linked-lists.py
+++ linked_lists/doubly-linked-lists.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self):
+    def __init__(self, val):
         self.val = val
         self.next = None
         self.prev = None
@@ -7,3 +7,19 @@ class Node:
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
+        self.size = 0
+
+    # Get value of the "nth" node according to the index
+    def get(self, index):
+        # Base/exit cases
+        if index < 0 or index >= self.size: return -1
+        if self.head is None: return -1
+        
+        # Set current node to head
+        cur = self.head
+        
+        # Stop loop at provided index then return its value
+        for i in range(index):
+            cur = cur.next
+
+        return cur.val
